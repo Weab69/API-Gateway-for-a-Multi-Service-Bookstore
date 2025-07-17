398e7ca25baeebf0634251b63fad207550580b88
Weab69 <yeabyg69@gmail.com>
2025-07-17 11:42:53 +0300

    Add login with winston

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv"
+import logger from "./utils/logger";
 
-const port = 8000
+dotenv.config();
+
+const port = process.env.PORT || 8000;
 
 const app: Express = express();
+
+app.use((req, res, next) => {
+    logger.info(`${req.method} ${req.url}`);
+    next()
+})
 
 app.get("/", (req: Request,res: Response) => {
     res.send("Hello from express + TS")
